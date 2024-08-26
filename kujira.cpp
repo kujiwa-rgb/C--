d1583cbf8705f8430c9dc2afa72a761b16c21a88
Kujiwa-rgb <advertisement00@outlook.com>
2024-08-26 13:08:50 -0500

    First Commit

---
+++ kujira.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Include input/output stream
+
+int main() {
+    std::cout << "Kujira is running... Welcome v0.0.1" << std::endl;  // Replace the text with your own statement
+    std::cin.get();  // Wait for user input before closing the program
+    return 0;  // Return 0 to indicate the program ended successfully
+}
